e69bb9e7b5d2e52ff670988cea043eb7239e835d
kylelambert001 <kylelambert001@gmail.com>
2020-03-21 22:27:38 +1030

    commit

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,15 +3,37 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => {
+        this.setState({ copied: false });
+      }, 1500);
+    });
+  }
   render() {
     const { name, background } = this.props;
     return (
       <div style={{ background }} className="ColorBox">
+        <div
+          style={{ background }}
+          className={
+            this.state.copied ? "copy-overlay show" : "copy-overlay"
+          }></div>
+        <div className={this.state.copied ? "copy-msg show" : "copy-msg"}>
+          <h1>Copied</h1>
+          <p>{background}</p>
+        </div>
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
           </div>
-          <CopyToClipboard text={background}>
+          <CopyToClipboard text={background} onCopy={this.changeCopyState}>
             <button className="copy-button">Copy</button>
           </CopyToClipboard>
           <span className="see-more">More</span>
